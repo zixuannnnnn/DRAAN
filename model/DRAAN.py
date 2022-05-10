42d820ba76e26d43f8bde6570086e4f04fbcd54b
zixuannnnnn <69336255+zixuannnnnn@users.noreply.github.com>
2022-05-10 21:20:33 +0800

    Add files via upload

---
+++ model/DRAAN.py
@@ -0,0 +1,135 @@
+## ECCV-2018-Image Super-Resolution Using Very Deep Residual Channel Attention Networks
+## https://arxiv.org/abs/1807.02758
+from model import common
+from attention import SGE,PA
+from model import ASPP
+import torch.nn as nn
+import torch
+def make_model(args, parent=False):
+    return DRAAN(args)
+
+
+
+
+
+class RB(nn.Module):
+    def __init__(
+        self, conv, n_feat, kernel_size, reduction,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+
+        super(RB, self).__init__()
+        modules_body = []
+        for i in range(2):
+            modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+            if bn: modules_body.append(nn.BatchNorm2d(n_feat))
+            if i == 0: modules_body.append(act)
+        self.body = nn.Sequential(*modules_body)
+        self.res_scale = res_scale
+        self.sge=SGE.SpatialGroupEnhance
+
+    def forward(self, x):
+        bs, c, h, w = x.shape
+        self.conv1d=nn.Conv2d(in_channels=2*c,out_channels=c,kernel_size=1,stride=1,bias=False)
+        self.pa = PA.Pixelattention(channel=c)
+        self.ASPP = ASPP.ASPP(in_channels=c,atrous_rates=3,out_channels=3)
+        res=ASPP(x)
+        pa=self.pa(res)
+        sge=self.sge(res)
+        out=torch.cat([pa,sge],1)
+        out=self.conv1d(out)
+
+        return out
+
+
+class AAG(nn.Module):
+    def __init__(self, conv, n_feat, kernel_size, reduction, act, res_scale, n_resblocks):
+        super(AAG, self).__init__()
+        modules_body = []
+        modules_body = [
+            RB(
+                conv, n_feat, kernel_size, reduction, bias=True, bn=False, act=nn.ReLU(True), res_scale=1) \
+            for _ in range(n_resblocks)]
+        modules_body.append(conv(n_feat, n_feat, kernel_size))
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res += x
+        return res
+
+
+class DRAAN(nn.Module):
+    def __init__(self, args, conv=common.default_conv):
+        super(DRAAN, self).__init__()
+        self.a = nn.Parameter(torch.Tensor([0]))
+        self.a.requires_grad=True
+        n_resgroups = args.n_resgroups
+        n_resblocks = args.n_resblocks
+        n_feats = args.n_feats
+        kernel_size = 3
+        reduction = args.reduction 
+        scale = args.scale[0]
+        act = nn.ReLU(True)
+        
+        # RGB mean for DIV2K
+        rgb_mean = (0.4488, 0.4371, 0.4040)
+        rgb_std = (1.0, 1.0, 1.0)
+        self.sub_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std)
+        
+        # define head module
+        modules_head = [conv(args.n_colors, n_feats, kernel_size)]
+
+        # define body module
+        modules_body = [
+            AAG(
+                conv, n_feats, kernel_size, reduction, act=act, res_scale=args.res_scale, n_resblocks=n_resblocks) \
+            for _ in range(n_resgroups)]
+        modules_body.append(conv(n_feats, n_feats, kernel_size))
+
+        # define tail module
+        modules_tail = [
+            common.Upsampler(conv, scale, n_feats, act=False),
+            conv(n_feats, args.n_colors, kernel_size)]
+
+        self.add_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std, 1)
+
+        self.head = nn.Sequential(*modules_head)
+        self.body = nn.Sequential(*modules_body)
+        self.tail = nn.Sequential(*modules_tail)
+
+    def forward(self, x):
+        x = self.sub_mean(x)
+        x = self.head(x)
+        res = self.body(x)
+        res += x
+
+        x = self.tail(res)
+        x = self.add_mean(x)
+
+        return x 
+
+    def load_state_dict(self, state_dict, strict=False):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('msa') or name.find('a') >= 0:
+                        print('Replace pre-trained upsampler to new one...')
+                    else:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('msa') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
+
+        if strict:
+            missing = set(own_state.keys()) - set(state_dict.keys())
+            if len(missing) > 0:
+                raise KeyError('missing keys in state_dict: "{}"'.format(missing))
